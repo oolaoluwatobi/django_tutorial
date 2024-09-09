9d9d95199540d63b08589c4ead80f6447f195655
oolaoluwatobi <oolaoluwatobi@gmail.com>
2024-09-09 09:53:03 +0100

    feat: Add post page and update post list template
    The code changes include adding a new post page template and updating the post list template to include a link to the post page. Additionally, the URLs for the posts app have been updated to include a named URL for the post list view.
    
    Note: The recent user commits and recent repository commits are not relevant for generating the commit message.

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
@@ -9,3 +10,12 @@ def posts_list(request):
     }
 
     return render(request, 'posts/posts_list.html', context)
+
+def post_page(request, slug):
+
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/post_page.html', context)
